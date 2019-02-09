dd6bb1441c618bee708a1c2a696d01dff1e635da
Aly Traore <alytraore@alys-macbook-pro.local>
2019-02-08 19:32:04 -0500

    Initial Commit

---
+++ Week0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week0
+//
+//  Created by Aly Traore on 2/8/19.
+//  Copyright © 2019 Aly Traore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
