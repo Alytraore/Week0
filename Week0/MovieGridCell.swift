74cb94671db3bc32915b76d5e8496ef9c36ad88b
Aly Traore <alytraore@alys-macbook-pro.local>
2019-02-28 16:28:01 -0500

    update

---
+++ Week0/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Week0
+//
+//  Created by Aly Traore on 2/27/19.
+//  Copyright © 2019 Aly Traore. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
